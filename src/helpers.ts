402f5e3431fce6d6279fbf12442f244ce14a7a19
Alex Bisaillion <a.bisaillion@gmail.com>
2021-04-17 16:17:01 -0400

    Add jest dev dependency

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,1 @@
-export const isNilOrEmpty = <T>(elements: T[] | undefined | null): boolean => !!elements && elements.length === 0;
+export const isNilOrEmpty = <T>(elements: T[] | undefined | null): boolean => !elements || elements.length === 0;
---
+++ src/test/helpers.test.ts
@@ -0,0 +1,16 @@
+import { isNilOrEmpty } from '../helpers';
+
+describe('isNilOrEmpty', () => {
+  it('returns true when there are no elements', () => {
+    expect(isNilOrEmpty([])).toBeTruthy();
+  });
+
+  it('returns false when there are elements', () => {
+    expect(isNilOrEmpty(['hello', 'there'])).toBeFalsy();
+  });
+
+  it('returns true when given a null argument', () => {
+    expect(isNilOrEmpty(undefined)).toBeTruthy();
+    expect(isNilOrEmpty(null)).toBeTruthy();
+  });
+});

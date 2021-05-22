96bb4f549f4450d6bff4d4ed3f387db77db7b8ab
Alex Bisaillion <a.bisaillion@gmail.com>
2021-05-22 13:43:19 -0400

    Remove old client from server directory

--- server/src/client/src/components/icons/Material.tsx
+++
@@ -1,9 +0,0 @@
-import Link from '@material-ui/icons/Link';
-import LinkOff from '@material-ui/icons/LinkOff';
-import NoteAdd from '@material-ui/icons/NoteAdd';
-import BackspaceIcon from '@material-ui/icons/Backspace';
-
-export const LinkEnabledIcon = () => <Link />;
-export const LinkDisabledIcon = () => <LinkOff />;
-export const CreateIcon = () => <NoteAdd />;
-export const StripIcon = () => <BackspaceIcon />;

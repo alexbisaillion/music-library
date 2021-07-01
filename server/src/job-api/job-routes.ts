c96188737fd0edb0ca7d86b24360b68406ec7488
Alex Bisaillion <a.bisaillion@gmail.com>
2021-06-30 21:29:22 -0400

    Protect job endpoints

--- server/src/job-api/job-routes.ts
+++ server/src/job-api/job-routes.ts
@@ -1,7 +1,21 @@
-import { Router } from 'express';
+import { Request, Response, NextFunction, Router } from 'express';
+import { environment } from '../helpers/environment';
+import { ErrorCode, sendError } from '../helpers/routing';
 import { handleRefreshPlays } from './job-handlers';
 
 export const jobRouter = Router();
 
 const routeURL = (route: string) => `/job/${route}`;
 jobRouter.post(routeURL('refreshPlays'), handleRefreshPlays);
+
+const handleValidateAuthorized = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  if (!req.body.secret) {
+    sendError(res, ErrorCode.Unauthorized, 'Provide the secret to access jobs.');
+  }
+  if (req.body.secret !== environment.variables.SECRET) {
+    sendError(res, ErrorCode.Unauthorized, 'Provided secret is invalid.');
+  }
+  next();
+};
+
+jobRouter.post('*', handleValidateAuthorized);
